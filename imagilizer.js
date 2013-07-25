04a53492d9b54af3c44f1e9db9e02b31422b3666
Youssif <abdulyoussif@gmail.com>
2013-07-25 14:51:37 -0700

    seed funding round nearly complete minus the refactoring and requested crossfading

--- imagilizer.js
+++ imagilizer.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 	$('img:first').addClass('active');
 	$('.active').clone().appendTo('#panel');
 	$('#panel .active').removeAttr('width');
+	var data_caption = $('#panel .active').data('caption');
+	$('.caption').html('<h2 class = "photo_label">'+data_caption+'</h2>');
 
 	$('#sidebar img').click(function(){
 		$('#panel .active').remove();
@@ -11,9 +13,9 @@ $(document).ready(function(){
 		$('.active').clone().hide().appendTo('#panel');
 		$('#panel .active').fadeIn(800);
 		$('#panel .active').removeAttr('width');
+		var data_caption = $('#panel .active').data('caption');
+		$('.caption').html('<h2 class = "photo_label">'+data_caption+'</h2>');
 	});
-
-
 
 
 
